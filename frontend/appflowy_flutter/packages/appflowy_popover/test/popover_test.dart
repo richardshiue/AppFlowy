1202c65a703e24a1a5286b4446aa22ef1f7c9378
Richard Shiue <71320345+richardshiue@users.noreply.github.com>
2023-09-13 22:36:33 +0800

    test: add widget test

--- frontend/appflowy_flutter/packages/appflowy_popover/test/popover_test.dart
+++ frontend/appflowy_flutter/packages/appflowy_popover/test/popover_test.dart
@@ -1,10 +1,107 @@
+import 'package:appflowy_popover/appflowy_popover.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('adds one to input values', () {
-    // final calculator = Calculator();
-    // expect(calculator.addOne(2), 3);
-    // expect(calculator.addOne(-7), -6);
-    // expect(calculator.addOne(0), 1);
+  testWidgets('create a popover', (tester) async {
+    await tester.pumpWidget(
+      PopoverTestWidget(
+        child: Popover(
+          triggerActions: PopoverTriggerFlags.click,
+          child: TextButton(
+            child: const Text("AppFlowy"),
+            onPressed: () {},
+          ),
+          popupBuilder: (context) => const SizedBox(height: 30, width: 30),
+        ),
+      ),
+    );
+
+    // press the button
+    await tester.tap(find.byType(TextButton));
+    await tester.pumpAndSettle();
+
+    // the popover should appear
+    expect(find.byType(PopoverContainer), findsOneWidget);
   });
+
+  testWidgets('dismiss the popover with esc', (tester) async {
+    await tester.pumpWidget(
+      PopoverTestWidget(
+        child: Popover(
+          triggerActions: PopoverTriggerFlags.click,
+          child: TextButton(
+            child: const Text("AppFlowy"),
+            onPressed: () {},
+          ),
+          popupBuilder: (context) => const SizedBox(height: 30, width: 30),
+        ),
+      ),
+    );
+
+    // tap button
+    await tester.tap(find.byType(TextButton));
+    await tester.pumpAndSettle();
+    expect(find.byType(PopoverContainer), findsOneWidget);
+
+    // press escape
+    await tester.sendKeyEvent(LogicalKeyboardKey.escape);
+    await tester.pumpAndSettle();
+
+    // popover is dismissed
+    expect(find.byType(PopoverContainer), findsNothing);
+  });
+
+  testWidgets('focus changes', (tester) async {
+    await tester.pumpWidget(
+      PopoverTestWidget(
+        child: Popover(
+          triggerActions: PopoverTriggerFlags.click,
+          child: TextButton(
+            child: const Text("AppFlowy"),
+            onPressed: () {},
+          ),
+          popupBuilder: (context) => Focus(
+            onKey: (node, event) {
+              if (event is RawKeyDownEvent &&
+                  event.logicalKey == LogicalKeyboardKey.escape) {
+                return KeyEventResult.handled;
+              }
+              return KeyEventResult.ignored;
+            },
+            child: const Material(child: TextField(autofocus: true)),
+          ),
+        ),
+      ),
+    );
+
+    // tap button
+    await tester.tap(find.byType(TextButton));
+    await tester.pumpAndSettle();
+    expect(find.byType(PopoverContainer), findsOneWidget);
+
+    // press escape
+    await tester.sendKeyEvent(LogicalKeyboardKey.escape);
+    await tester.pumpAndSettle();
+
+    // focus should absorb the escape and the popover should persist
+    expect(find.byType(PopoverContainer), findsOneWidget);
+  });
+}
+
+@visibleForTesting
+class PopoverTestWidget extends StatelessWidget {
+  final Widget child;
+  const PopoverTestWidget({super.key, required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Popover Test",
+      home: Scaffold(
+        body: Center(child: child),
+      ),
+    );
+  }
 }
